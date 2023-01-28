eb722b220b3d3717fa0ac4fe55c647979f1b5614
Mauricio F <mauriciofsnts@gmail.com>
2023-01-27 22:34:21 -0300

    refactor(seo): move seo to index

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,20 +11,6 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Kanit:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
           />
-
-          <meta name="title" content="Mauricio Ferraz - software engineer" />
-          <meta
-            name="description"
-            content="Desenvolvedor Front-end com experiência em React.js, Next.js, Redux, JavaScript, TypeScript, Styled Components, Material UI"
-          />
-          <meta
-            name="keywords"
-            content="Mauricio, Ferraz, Mauricio Ferraz, software, frontend, engineer, software engineer, dev, redux, react, next, javascript, typescript"
-          />
-          <meta name="robots" content="index, follow" />
-          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-          <meta name="language" content="Portuguese" />
-          <meta name="author" content="Mauricio Ferraz" />
         </Head>
         <body>
           <Main />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
 import { Button, Card, Progress, Showcase } from '../components'
 import Contact from '../components/contact'
 
@@ -9,6 +10,20 @@ const Home: NextPage = () => {
   return (
     <div className={styles.main}>
       <Head>
+        <title>Mauricio Ferraz</title>
+        <meta
+          name="description"
+          content="Mauricio Ferraz - Software engineer"
+        />
+        <meta
+          name="keywords"
+          content="Mauricio, Ferraz, Mauricio Ferraz, software, frontend, engineer, software engineer, dev, redux, react, next, javascript, typescript"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="Portuguese" />
+        <meta name="author" content="Mauricio Ferraz" />
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
